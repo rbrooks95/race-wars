20b607b7d45b8c533e5fd8df3f6915d6d00a6eae
Roshawn Brooks <69084474+rbrooks95@users.noreply.github.com>
2022-07-29 09:31:16 -0400

    fixed dropdown

--- client/src/components/pages/RaceForm.js
+++ client/src/components/pages/RaceForm.js
@@ -70,8 +70,8 @@ export default function RaceForm(props) {
     setFormState({ ...formState, [e.target.id]: e.target.value });
   };
 
-  const handleChangeDrop = (e, { r }) => {
-    setFormState({ raceType });
+  const handleChangeDrop = (e, { ...formState }) => {
+    setFormState({ trackOptions });
   };
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -97,6 +97,27 @@ export default function RaceForm(props) {
           </Form.Field>
 
           <Form.Field>
+            <label htmlFor="trackPhoto"> Tracks</label>
+            <select id="trackPhoto" onChange={handleChange}>
+              <option value="https://maxvelocity.events/wp-content/uploads/2019/04/Estoril-Portugal.jpg">
+                Coral Springs
+              </option>
+              <option value="https://phantom-marca.unidadeditorial.es/129f786ee94996750e0756971c33a704/resize/1320/f/jpg/assets/multimedia/imagenes/2021/09/09/16312120805110.jpg">
+                MONZA
+              </option>
+              <option value="https://www.co.monterey.ca.us/home/showpublishedimage/25376/637123523806970000">
+                LAGUNA SECA
+              </option>
+              <option value="https://www.thedrive.com/content/2020/02/DSC01209.jpg?quality=85">
+                MOUNT PANORAMA
+              </option>
+              <option value="https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Fbeyondtheflag.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2018%2F04%2F690124314-f1-grand-prix-of-monaco.jpg.jpg">
+                CIRCUIT DE MONACO
+              </option>
+            </select>
+          </Form.Field>
+
+          {/* <Form.Field>
             <label htmlFor="trackPhoto">Track</label>
             <Dropdown
               placeholder="Select Track"
@@ -121,7 +142,7 @@ export default function RaceForm(props) {
               onChange={handleChange}
               id="raceType"
             />
-          </Form.Field>
+          </Form.Field> */}
           <Button type="submit">Submit</Button>
         </Form>
       </Container>
--- client/src/components/pages/ViewTracks.js
+++ client/src/components/pages/ViewTracks.js
@@ -1,7 +1,50 @@
+import { Card, Button, Header, Image, Modal } from "semantic-ui-react";
+import { useState } from "react";
+
 export default function ViewTracks() {
+  const [open, setOpen] = useState(false);
   return (
     <div>
       <h1>VIEW THE TRACKS</h1>
+
+      <Card
+        image="https://maxvelocity.events/wp-content/uploads/2019/04/Estoril-Portugal.jpg"
+        header="Coral Springs"
+      />
+      <Modal
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+        trigger={
+          <Card
+            image="https://maxvelocity.events/wp-content/uploads/2019/04/Estoril-Portugal.jpg"
+            header="Coral Springs"
+          />
+        }
+      >
+        <Modal.Header textAlign="center">Coral Springs</Modal.Header>
+        <Modal.Content image>
+          <Image
+            size="large"
+            src="https://maxvelocity.events/wp-content/uploads/2019/04/Estoril-Portugal.jpg"
+            fluid
+            wrapped
+            onClick={() => setOpen(false)}
+          />
+        </Modal.Content>
+        <Modal.Actions>
+          {/* <Button color="black" onClick={() => setOpen(false)}>
+            Nope
+          </Button>
+          <Button
+            content="Yep, that's me"
+            labelPosition="right"
+            icon="checkmark"
+            onClick={() => setOpen(false)}
+            positive
+          /> */}
+        </Modal.Actions>
+      </Modal>
     </div>
   );
 }
