015247ae9bf8ca452ba7094f42332978fe3d52fd
Roshawn Brooks <69084474+rbrooks95@users.noreply.github.com>
2022-08-07 01:02:31 -0400

    ui for update posted

--- client/src/components/pages/ViewRacers.js
+++ client/src/components/pages/ViewRacers.js
@@ -1,9 +1,10 @@
-import { Card, Icon, Image, Button } from "semantic-ui-react";
+import { Card, Icon, Image, Button, Popup, Form } from "semantic-ui-react";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
 export default function ViewRacers() {
   const [info, setinfo] = useState([]);
+  const [formState, setFormState] = useState("");
   useEffect(() => {
     const racers = async () => {
       const res = await axios.get("http://localhost:3001/races");
@@ -13,10 +14,9 @@ export default function ViewRacers() {
     racers();
   }, []);
 
-  const id = info.map((res) => {
-    return res._id;
-  });
-  console.log(id);
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.id]: e.target.value });
+  };
 
   const remove = async (e) => {
     await axios.delete(`http://localhost:3001/delete/${e}`);
@@ -37,6 +37,53 @@ export default function ViewRacers() {
               <Card.Header>{res.model}</Card.Header>
               <Card.Header>{res.raceType}</Card.Header>
               <Button onClick={() => remove(res._id)}>Delete</Button>
+              <Popup
+                content={
+                  <Form>
+                    <Form.Field>
+                      <label htmlFor="trackPhoto"> Tracks</label>
+                      <select
+                        id="trackPhoto"
+                        onChange={handleChange}
+                        value={formState.trackPhoto}
+                      >
+                        <option value="https://maxvelocity.events/wp-content/uploads/2019/04/Estoril-Portugal.jpg">
+                          Coral Springs
+                        </option>
+                        <option value="https://phantom-marca.unidadeditorial.es/129f786ee94996750e0756971c33a704/resize/1320/f/jpg/assets/multimedia/imagenes/2021/09/09/16312120805110.jpg">
+                          MONZA
+                        </option>
+                        <option value="https://www.co.monterey.ca.us/home/showpublishedimage/25376/637123523806970000">
+                          LAGUNA SECA
+                        </option>
+                        <option value="https://www.thedrive.com/content/2020/02/DSC01209.jpg?quality=85">
+                          MOUNT PANORAMA
+                        </option>
+                        <option value="https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Fbeyondtheflag.com%2Fwp-content%2Fuploads%2Fgetty-images%2F2018%2F04%2F690124314-f1-grand-prix-of-monaco.jpg.jpg">
+                          CIRCUIT DE MONACO
+                        </option>
+                      </select>
+                    </Form.Field>
+                    <Form.Field>
+                      <label htmlFor="raceType">Race Type</label>
+                      <select
+                        id="raceType"
+                        onChange={handleChange}
+                        value={formState.raceType}
+                      >
+                        <option value="Drift">Drift</option>
+                        <option value="circuit">Circuit</option>
+                        <option value="time attack">Time Attack</option>
+                        <option value="sprint">Sprint</option>
+                        <option value="Drag">Drag</option>
+                      </select>
+                    </Form.Field>
+                  </Form>
+                }
+                on="click"
+                popper={{ id: "popper-container", style: { zIndex: 2000 } }}
+                trigger={<Button>Open a popup</Button>}
+              />
             </Card.Content>
           </Card>
         ))}
